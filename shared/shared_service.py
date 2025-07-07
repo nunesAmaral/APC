20d12372b51001c1e4aa2b66a7dc8d464b54e161
João Vitor Nunes do Amaral <joaovitornunesdoamaral@outlook.com>
2025-07-07 08:50:53 -0300

    primeiro commit

---
+++ shared/shared_service.py
@@ -0,0 +1,9 @@
+material_selecionado = None;
+
+def get_material_selecionado():
+    return material_selecionado
+
+def set_material_selecionado(material):
+    global material_selecionado
+    material_selecionado = material
+
